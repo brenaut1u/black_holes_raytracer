e3119a321a06e1a0dfef62e3275b1fcaba01356b
AndyPanMcGill <zhouyang.pan@mail.mcgill.ca>
2024-12-03 16:16:29 -0500

    Add files via upload

--- PostNewtonian.py
+++ PostNewtonian.py
@@ -23,7 +23,8 @@ def compute_accelerations_and_jerks(positions, velocities, masses):
     n = len(masses)
     accelerations = np.zeros_like(positions)
     jerks = np.zeros_like(positions)
-
+    
+    r_lim=1e9
     for i in prange(n):
         ai = np.zeros(3)
         ji = np.zeros(3)
@@ -31,18 +32,19 @@ def compute_accelerations_and_jerks(positions, velocities, masses):
         for j in range(n):
             if i != j:
                 # Relative vectors
-                x_ij = positions[i] - positions[j]
-                v_ij = velocities[i] - velocities[j]
+                x_ij = positions[i,:] - positions[j,:]
+                v_ij = velocities[i,:] - velocities[j,:]
                 r_ij2 = x_ij@x_ij
                 r_ij = np.sqrt(r_ij2)
+
 
                 # Newtonian acceleration
                 ai -= G * masses[j] * x_ij / r_ij**3
 
                 # 1PN corrections 
-                vi2 = velocities[i] @ velocities[i]
-                vj2 = velocities[j] @ velocities[j]
-                vi_dot_vj=velocities[i] @ velocities[j]
+                vi2 = velocities[i,:] @ velocities[i,:]
+                vj2 = velocities[j,:] @ velocities[j,:]
+                vi_dot_vj=velocities[i,:] @ velocities[j,:]
                 vij_dot_xij = v_ij @ x_ij
                 vj_dot_nij = velocities[j] @ (x_ij/r_ij)
 
@@ -55,19 +57,21 @@ def compute_accelerations_and_jerks(positions, velocities, masses):
                     2 * vj2 +
                     1.5*(vj_dot_nij**2)
                 )
-                pn_correction_ij += np.dot(x_ij,4*velocities[i]-3*velocities[j])*v_ij / r_ij**3
+                pn_correction_ij += np.dot(x_ij,4*velocities[i,:]-3*velocities[j,:])*v_ij / r_ij**3
                 a_pn = pn_correction_ij * G * masses[j] / c**2
                 
                 # calculate cross-terms
                 for k in range(n):
                     if k != i and k != j:
                         # Relative vectors
-                        x_jk = positions[j] - positions[k]
-                        x_ik = positions[i] - positions[k]
+                        x_jk = positions[j,:] - positions[k,:]
+                        x_ik = positions[i,:] - positions[k,:]
                         r_jk2 = x_jk @ x_jk
                         r_jk = np.sqrt(r_jk2)
+
                         r_ik2 = x_ik @ x_ik
                         r_ik = np.sqrt(r_ik2)
+
                         pn_correction_ik = G * masses[k] * x_ij / r_ij**3 * (
                             1 / r_jk +
                             + 4 / r_ik
@@ -81,8 +85,8 @@ def compute_accelerations_and_jerks(positions, velocities, masses):
                 # dai/dt=G*mj*(-dr_vec/dt/r^3+3*dr_scal/dt*r_vec/r^4)
                 ji += G * masses[j] * (-v_ij / r_ij**3 + 3 * vij_dot_xij * x_ij / r_ij**5)
 
-        accelerations[i] = ai
-        jerks[i] = ji
+        accelerations[i,:] = ai
+        jerks[i,:] = ji
 
     return accelerations, jerks
 
@@ -104,7 +108,8 @@ def hermite_integrator(positions, velocities, masses, dt):
     velocities += 0.5 * (accelerations + accelerations_new) * dt + (1 / 12) * (jerks - jerks_new) * dt**2
     speed = np.sqrt(np.sum(velocities**2,axis=1))
     speed_capped = np.clip(speed,0,c)
-    velocities *= speed_capped / speed
+    for i in prange(len(speed)): 
+        velocities[i,:] *= speed_capped[i] / speed[i]
     positions += 0.5 * (velocities + velocities_old) * dt + (1 / 12) * (accelerations - accelerations_new) * dt**2
     
     return positions, velocities
@@ -157,7 +162,7 @@ def simulate(n_bodies, time_steps, dt):
         ax.set_xlim(-1e11, 1e11)
         ax.set_ylim(-1e11, 1e11)
         ax.set_zlim(-1e11, 1e11)
-        plt.autoscale(False)
+        #plt.autoscale(False)
 
         # Pause to create the effect of real-time animation
         plt.pause(0.01)
