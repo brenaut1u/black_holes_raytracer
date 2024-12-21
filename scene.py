d1a83af5f689e46f2d1e0da18144ee32e69272ae
Wenhao-Shen <wenhaomtl@gmail.com>
2024-12-20 23:27:40 -0500

    Add files via upload

--- scene.py
+++ scene.py
@@ -11,14 +11,18 @@ class Scene:
                              np.asarray([0, 0, -1e9]),
                              0.3,
                              1,
-                             700,
+                             500,
                              16 / 9,
                              background_image,
                              1e10)
 
-        self.positions = np.asarray([[-2e8, 0., -1e9], [2e8, 0., -1e9]])
-        self.velocities = np.asarray([[0., 1e5, 0.], [0., -1e5, 0.]])
-        self.masses = np.asarray([2e35, 4e35])
+        # self.positions = np.asarray([[-2e8, 0., -1e9], [2e8, 0., -1e9]])
+        # self.velocities = np.asarray([[0., 1e5, 0.], [0., -1e5, 0.]])
+        # self.masses = np.asarray([2e35, 4e35])
+
+        self.positions = np.asarray([[-2e8, 0., -1e9], [2e8, 0., -1e9], [6e8, 0., -1e9]])
+        self.velocities = np.asarray([[-1e5, 1e5, 0.], [-1e5, 0e5, 0], [-1e5, -1e5, 0.]])
+        self.masses = np.asarray([1e35, 2e35, 1e35])
 
         self.accelerations_pert_old = np.zeros(self.positions.shape, dtype=np.float64)
 
@@ -27,12 +31,10 @@ class Scene:
             len(self.masses), self.masses, dt, self.positions, self.velocities, self.accelerations_pert_old
         )
 
-    def render_animation(self, nb_frames, dt, part=-1):
-        # If part == -1, we render the full image. Otherwise, we only render a third of it (to allow to split up the render)
-
+    def render_animation(self, nb_frames, dt):
         for frame in range(nb_frames):
             print("Frame", frame)
-            img = self.camera.render(self.positions, self.velocities, self.masses, 0.1, part=part)
+            img = self.camera.render(self.positions, self.velocities, self.masses, 0.1)
             image.imsave('out/frame%02d.png' % frame, img)
 
             self.update_scene(dt)
